e5fe4da1c07ab9d3b7f94c5030db04c78fbe2ef1
Jesus Villota <danielvillota1016@gmail.com>
2019-02-20 12:40:03 -0500

    Geometric Shapes project created

---
+++ src/model/GeometricShapes.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.ArrayList;
+
+public class GeometricShapes {
+	
+	private ArrayList<Triangle> triangles;
+	
+	public GeometricShapes() {
+		this.triangles = new ArrayList<Triangle>();
+	}
+}
